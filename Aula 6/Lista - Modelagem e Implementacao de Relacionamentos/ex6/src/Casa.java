5d6c5a96166859a44e555ac655bef87d69d5b6cd
Gustavo <gustavogomes1108@gmail.com>
2025-08-26 12:34:02 -0300

    .

--- Aula 6/Lista - Modelagem e Implementacao de Relacionamentos/ex6/src/Casa.java
+++ Aula 6/Lista - Modelagem e Implementacao de Relacionamentos/ex6/src/Casa.java
@@ -3,9 +3,34 @@ public class Casa {
 
     Comodo[] comodos;
 
-    Casa(String endereco, String nomeComodo){
+    Casa(String endereco, String nomeComodo, int tamanho){
         this.endereco = endereco;
+
+        this.comodos = new Comodo[tamanho];
         comodos[0] = new Comodo(nomeComodo);
     }
 
+    void adicionarComodo(String nome){
+        int i = 0;
+
+        while (i < comodos.length && comodos[i] != null){
+            i++;
+        }
+
+        if (i == comodos.length){
+            System.out.println("Sem espaço para comodos!");
+        } else {
+            comodos[i] = new Comodo(nome);
+        }
+    }
+
+    void mostrarComodos(){
+        int i = 0;
+
+        while (i < comodos.length && comodos[i] != null){
+            System.out.println("Comodo" + (i+1) + ": " + comodos[i].nome);
+
+            i++;
+        }
+    }
 }
--- Aula 6/Lista - Modelagem e Implementacao de Relacionamentos/ex6/src/Main.java
+++ Aula 6/Lista - Modelagem e Implementacao de Relacionamentos/ex6/src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+        Casa casa = new Casa("Rua 2", "Quarto", 3);
+
+        casa.mostrarComodos();
+
+        casa.adicionarComodo("Cozinha");
+        casa.adicionarComodo("Quintal");
+        casa.adicionarComodo("Banheiro");
+
+        casa.mostrarComodos();
+    }
 }
